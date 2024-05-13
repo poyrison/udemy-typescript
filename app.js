26b0848d8b0780e6e8a39047966efcacb0fa51dc
김준식 <wnstlr8879@naver.com>
2024-05-14 00:58:13 +0900

    test

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("Hello world!");
+"use strict";
+const Test = (sum1, sum2) => {
+    const result = sum1 + sum2;
+    try {
+        return alert(result);
+    }
+    catch (err) {
+        return alert(err);
+    }
+};
--- app.ts
+++ app.ts
@@ -1,1 +1,8 @@
-console.log("Hello world!");
+const Test = (sum1: number, sum2: number) => {
+  const result = sum1 + sum2;
+  try {
+    return alert(result);
+  } catch (err) {
+    return alert(err);
+  }
+};
